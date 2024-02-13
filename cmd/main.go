498966ad90b8631a0e4c0d2fa904ba92683c1b3f
JPauloMoura <joaopaulo.034a@gmail.com>
2024-02-12 23:02:21 -0300

    feat: create server with routes

---
+++ cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/JPauloMoura/rinha-backend-q1-2024/internal/handler"
+	"github.com/JPauloMoura/rinha-backend-q1-2024/internal/repository"
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Panic("failed to loading .env file")
+	}
+
+	defineLogger()
+	repository.ConnectDB()
+
+	router := chi.NewMux()
+	router.Post("/clientes/{id}/transacoes", handler.CreateTransaction)
+	router.Get("/clientes/{id}/extrato", handler.GenerateExtract)
+
+	slog.Info("server is running...")
+	http.ListenAndServe(":3001", router)
+}
+
+func defineLogger() {
+	l := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+	})
+
+	slog.SetDefault(slog.New(l))
+}
