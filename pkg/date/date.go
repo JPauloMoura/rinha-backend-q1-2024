2637e326ea72143222b63b00e44a41d2a8efbcd8
JPauloMoura <joaopaulo.034a@gmail.com>
2024-02-15 19:21:35 -0300

    feat: add pkg date

--- pkg/date/date.go
+++ pkg/date/date.go
@@ -1,3 +1,18 @@
 package date
 
+import (
+	"log"
+	"time"
+	_ "time/tzdata"
+)
+
 var DATE_BR_WITH_HOURS = "02/01/2006 03:04:05.00"
+
+func LocationBR() *time.Location {
+	loc, err := time.LoadLocation("America/Sao_Paulo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return loc
+}
