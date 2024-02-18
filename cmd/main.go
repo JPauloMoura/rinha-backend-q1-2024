f63dee277e2ac59e51aef36174b0398e1f2c8706
JPauloMoura <joaopaulo.034a@gmail.com>
2024-02-18 12:25:47 -0300

    feat: add profiler

--- cmd/main.go
+++ cmd/main.go
@@ -5,20 +5,26 @@ import (
 	"net/http"
 	"os"
 
+	// _ "net/http/pprof"
+
 	"github.com/JPauloMoura/rinha-backend-q1-2024/internal/handler"
 	"github.com/JPauloMoura/rinha-backend-q1-2024/internal/repository"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	defineLogger()
+	// defineLogger()
 	repository.ConnectDB()
 
 	router := chi.NewMux()
-	router.Use(middleware.Recoverer)
 	router.Post("/clientes/{id}/transacoes", handler.CreateTransaction)
 	router.Get("/clientes/{id}/extrato", handler.GenerateExtract)
+
+	if os.Getenv("PPROF") != "" {
+		go func() {
+			http.ListenAndServe(":3333", nil)
+		}()
+	}
 
 	slog.Info("server is running...")
 	http.ListenAndServe(":3001", router)
@@ -27,7 +33,7 @@ func main() {
 func defineLogger() {
 	l := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		AddSource: true,
-		Level:     slog.LevelWarn,
+		Level:     slog.LevelDebug,
 	})
 
 	slog.SetDefault(slog.New(l))
