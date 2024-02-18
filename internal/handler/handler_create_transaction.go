52773499c677c0cdc3fab1099fef837fa2c99493
JPauloMoura <joaopaulo.034a@gmail.com>
2024-02-18 12:27:10 -0300

    refact: remove logs

--- internal/handler/handler_create_transaction.go
+++ internal/handler/handler_create_transaction.go
@@ -1,21 +1,23 @@
 package handler
 
 import (
+	"context"
 	"encoding/json"
-	"log/slog"
 	"net/http"
 	"strconv"
 	"strings"
 
 	"github.com/JPauloMoura/rinha-backend-q1-2024/internal/entities"
 	"github.com/JPauloMoura/rinha-backend-q1-2024/internal/service"
+	errors "github.com/JPauloMoura/rinha-backend-q1-2024/pkg/errors"
 	"github.com/go-chi/chi/v5"
 )
 
 func CreateTransaction(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(chi.URLParam(r, "id"))
-	if err != nil {
-		slog.Debug(err.Error())
+	_, exist := Ids[id]
+	if err != nil || !exist {
+		// slog.Debug("id not found")
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
@@ -23,7 +25,7 @@ func CreateTransaction(w http.ResponseWriter, r *http.Request) {
 	var t entities.Transaction
 	err = json.NewDecoder(r.Body).Decode(&t)
 	if err != nil {
-		slog.Debug(err.Error())
+		// slog.Debug(err.Error())
 		w.WriteHeader(http.StatusUnprocessableEntity)
 		return
 	}
@@ -31,24 +33,19 @@ func CreateTransaction(w http.ResponseWriter, r *http.Request) {
 	t.Type = strings.ToLower(t.Type)
 	err = t.Validate()
 	if err != nil {
-		slog.Debug(err.Error())
+		// slog.Debug(err.Error())
 		w.WriteHeader(http.StatusUnprocessableEntity)
 		return
 	}
 
-	resp, err := service.CreateTransaction(id, t)
-	if err != nil && err.Error() == "client not found" {
+	resp, err := service.CreateTransaction(context.TODO(), id, t)
+	if err == errors.ErrClientNotFound {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	if err != nil && err.Error() == "transaction invalid" {
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		return
-	}
-
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(http.StatusUnprocessableEntity)
 		return
 	}
 
--- internal/handler/handler_generate_extract.go
+++ internal/handler/handler_generate_extract.go
@@ -1,32 +1,44 @@
 package handler
 
 import (
+	"context"
 	"encoding/json"
-	"log/slog"
+
+	// "log/slog"
 	"net/http"
 	"strconv"
 
 	"github.com/JPauloMoura/rinha-backend-q1-2024/internal/service"
+	errors "github.com/JPauloMoura/rinha-backend-q1-2024/pkg/errors"
 	"github.com/go-chi/chi/v5"
 )
 
+var Ids = map[int]bool{
+	1: true,
+	2: true,
+	3: true,
+	4: true,
+	5: true,
+}
+
 func GenerateExtract(w http.ResponseWriter, r *http.Request) {
+
 	id, err := strconv.Atoi(chi.URLParam(r, "id"))
-	if err != nil {
-		slog.Debug(err.Error())
+	_, exist := Ids[id]
+	if err != nil || !exist {
+		// slog.Debug("id not found")
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	extract, err := service.GenerateExtract(id)
-	if err != nil && err.Error() == "client not found" {
+	extract, err := service.GenerateExtract(context.TODO(), id)
+	if err == errors.ErrClientNotFound {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
 	if err != nil {
-		slog.Debug(err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(http.StatusUnprocessableEntity)
 		return
 	}
 
