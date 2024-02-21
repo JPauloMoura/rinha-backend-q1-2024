0f583dff748fa9dcd5a6f544e9fdb7dee6fa7455
JPauloMoura <joaopaulo.034a@gmail.com>
2024-02-21 17:17:33 -0300

    chore: adjust infra settings

--- init.sql
+++ init.sql
@@ -1,26 +1,30 @@
-CREATE TABLE IF NOT EXISTS clientes (
+CREATE TABLE IF NOT EXISTS client_account (
     id smallint PRIMARY KEY,
-    nome VARCHAR(23),
-    limite integer,
-    saldo integer
+    name VARCHAR(23),
+    acc_limit integer,
+    balance integer,
+    generation integer
 );
 
-CREATE TABLE IF NOT EXISTS transactions (
-    clientId smallint references clientes(id),
-    valor integer,
-    tipo char(1),
-    descricao varchar(10),
-    realizada_em TIMESTAMP WITH TIME ZONE DEFAULT timezone('America/Sao_Paulo'::text, now())
+CREATE TABLE IF NOT EXISTS transaction (
+    id SERIAL PRIMARY KEY,
+    account_id smallint references client_account(id),
+    value integer,
+    type char(1),
+    description varchar(10),
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_client_account FOREIGN KEY (account_id) REFERENCES client_account(id)
 );
 
-INSERT INTO clientes (id, nome, limite, saldo)
+INSERT INTO client_account (id, name, acc_limit, balance, generation)
 VALUES
-    (1, 'o barato sai caro', 1000 * 100, 0),
-    (2, 'zan corp ltda', 800 * 100, 0),
-    (3, 'les cruders', 10000 * 100, 0),
-    (4, 'padaria joia de cocaia', 100000 * 100, 0),
-    (5, 'kid mais', 5000 * 100, 0);
+    (1, 'o barato sai caro', 1000 * 100, 0, 1),
+    (2, 'zan corp ltda', 800 * 100, 0, 1),
+    (3, 'les cruders', 10000 * 100, 0, 1),
+    (4, 'padaria joia de cocaia', 100000 * 100, 0, 1),
+    (5, 'kid mais', 5000 * 100, 0, 1);
 
 
-CREATE INDEX IF NOT EXISTS idx_clientes_id ON clientes (id);
-CREATE INDEX IF NOT EXISTS idx_transactions_realizada_em ON transactions (realizada_em);
+CREATE INDEX IF NOT EXISTS idx_client_account_id ON client_account (id);
+CREATE INDEX IF NOT EXISTS idx_client_account_generation ON client_account (generation);
+CREATE INDEX IF NOT EXISTS idx_transaction_account_id_created_at ON transaction (account_id, created_at DESC);
