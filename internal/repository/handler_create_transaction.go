99c5eb9dfc1e54a7e4ef9072dafef7192302561b
JPauloMoura <joaopaulo.034a@gmail.com>
2024-02-21 17:24:44 -0300

    feat: implement implements optimistic locking

--- internal/repository/handler_create_transaction.go
+++ internal/repository/handler_create_transaction.go
@@ -2,81 +2,92 @@ package repository
 
 import (
 	"context"
-	"fmt"
-	"time"
 
 	"github.com/JPauloMoura/rinha-backend-q1-2024/internal/entities"
-	"github.com/JPauloMoura/rinha-backend-q1-2024/pkg/errors"
 )
 
-func timeTrack(start time.Time, name string) {
-	elapsed := time.Since(start)
-	fmt.Println("==> ", name, elapsed.Milliseconds())
+type CreateTransactionResponse struct {
+	Balance int `json:"saldo"`
+	Limit   int `json:"limite"`
 }
 
-func CreateTransaction(ctx context.Context, clientId int, transaction entities.Transaction) (*CreateTransactionResponse, error) {
-	tx, err := DB.Begin(ctx)
-	if err != nil {
-		// slog.Error("Error starting transaction: ", err)
-		return nil, err
+func (h Repo) CreateTransaction(ctx context.Context, accId int, transaction entities.Transaction) (*CreateTransactionResponse, error) {
+	var (
+		balance       int
+		limit         int
+		generation    int
+		clientAccount entities.ClientAccount
+	)
+	// Lock otimista
+	// Buscamos as informações da conta juntamente com sua generation
+	// e quando formos atualiza o novo salvo só atualizamos
+	// se a generation que iremos atuliza for a mesma que realizamos a leitura
+	// caso contrario retentemos novamente em cima da nova generation
+	// como temos muita concorrencia, tentamos esse metódo umas 100x hehehe
+	var errorLock error
+	for retry := 0; retry < 100; retry++ {
+
+		errorLock = h.DB.Connection.QueryRow(ctx, `
+			SELECT balance, acc_limit, generation FROM client_account
+			WHERE id = $1`, accId,
+		).Scan(&balance, &limit, &generation)
+
+		if errorLock != nil {
+			// fmt.Println("select ", errorLock)
+			break
+		}
+
+		clientAccount = entities.ClientAccount{
+			Balance:    balance,
+			Limit:      limit,
+			Generation: generation,
+		}
+
+		errorLock = clientAccount.SetNewBalanceByTransaction(transaction)
+		if errorLock != nil {
+			// fmt.Println("set ", errorLock, clientAccount)
+			break
+		}
+
+		cmd, errorLock := h.DB.Connection.Exec(ctx, `
+			UPDATE client_account SET 
+			balance = $1, generation = generation+1
+			WHERE id = $2 and generation = $3`,
+			clientAccount.Balance, accId, clientAccount.Generation,
+		)
+
+		// no update
+		if cmd.RowsAffected() <= 0 || errorLock != nil {
+			// fmt.Println("update ", errorLock, clientAccount)
+			errorLock = nil
+			continue
+		}
+
+		// fmt.Println("break ", errorLock, clientAccount, cmd.RowsAffected())
+
+		break
+
+		// fmt.Printf("client: %d v%d loopError:%d\n erro:%v", clientId, versao, retry, errorLock)
 	}
 
-	var (
-		clientSaldo  int
-		clientLimite int
+	if errorLock != nil {
+		// fmt.Println("error lock ", errorLock)
+		return nil, errorLock
+	}
+
+	_, err := h.DB.Connection.Exec(ctx, `
+		INSERT INTO transaction (account_id, value, type, description)
+		VALUES ($1, $2, $3, $4)`,
+		accId, transaction.Value, transaction.Type, transaction.Description,
 	)
 
-	err = tx.QueryRow(ctx, "SELECT saldo, limite FROM clientes WHERE id = $1 FOR UPDATE", clientId).
-		Scan(&clientSaldo, &clientLimite)
-
 	if err != nil {
-		// slog.Error("client not found", err)
-		return nil, err
-	}
-
-	switch transaction.Type {
-	case "c":
-		clientSaldo += transaction.Value
-	case "d":
-		clientSaldo -= transaction.Value
-	}
-
-	if clientSaldo < (clientLimite * -1) {
-		return nil, errors.ErrInvalidTransaction
-	}
-
-	defer func() {
-		if err != nil {
-			tx.Rollback(ctx)
-			// slog.Error("Transaction rolled back: ", err)
-		}
-	}()
-
-	_, err = tx.Exec(ctx, "INSERT INTO transactions (clientId, valor, tipo, descricao) VALUES ($1, $2, $3, $4)", clientId, transaction.Value, transaction.Type, transaction.Description)
-	if err != nil {
-		// slog.Error(err.Error())
-		return nil, err
-	}
-
-	_, err = tx.Exec(ctx, "UPDATE clientes SET saldo = $1 WHERE id = $2", clientSaldo, clientId)
-	if err != nil {
-		// slog.Error(err.Error())
-		return nil, err
-	}
-
-	err = tx.Commit(ctx)
-	if err != nil {
-		// slog.Error("Error committing transaction: ", err)
+		// fmt.Println("error insert transaction ", err)
 		return nil, err
 	}
 
 	return &CreateTransactionResponse{
-		Saldo: clientSaldo,
-		Limit: clientLimite,
+		Balance: clientAccount.Balance,
+		Limit:   clientAccount.Limit,
 	}, nil
-}
-
-type CreateTransactionResponse struct {
-	Saldo int `json:"saldo"`
-	Limit int `json:"limite"`
 }
